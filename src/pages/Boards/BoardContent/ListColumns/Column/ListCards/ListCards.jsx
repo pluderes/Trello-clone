e0a61d6fc6acde4b89a300891433b95f6da9e2d5
TrungTD <kingofpoppro@gmail.com>
2024-04-14 17:34:56 +0700

    feat: add drag ovetlay

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,30 +1,39 @@
 import { Box } from "@mui/material";
 import React from "react";
 import CardItem from "./CardItem/CardItem";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 
 function ListCards(props) {
   const { cards } = props;
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 1,
-        p: "0 5px",
-        m: "0 5px",
-        overflowX: "hidden",
-        overflowY: "auto",
-        maxHeight: (theme) =>
-          `calc(${theme.trolle.boardContentHeight} - 
-        ${theme.spacing(5)} - 
-        ${theme.trolle.cardHeaderHeight} - 
-        ${theme.trolle.cardFooterHeight})`,
-      }}
+    <SortableContext
+      items={cards?.map((el) => el?._id)}
+      strategy={verticalListSortingStrategy}
     >
-      {cards?.map((item, index) => (
-        <CardItem key={index} detailCard={item} />
-      ))}
-    </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 1,
+          p: "0 5px",
+          m: "0 5px",
+          overflowX: "hidden",
+          overflowY: "auto",
+          maxHeight: (theme) =>
+            `calc(${theme.trolle.boardContentHeight} - 
+            ${theme.spacing(5)} - 
+            ${theme.trolle.cardHeaderHeight} - 
+            ${theme.trolle.cardFooterHeight})`,
+        }}
+      >
+        {cards?.map((item, index) => (
+          <CardItem key={index} detailCard={item} />
+        ))}
+      </Box>
+    </SortableContext>
   );
 }
 
